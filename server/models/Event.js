f67c0a96cbf8fa591a8f0fa06a195c796df29b0e
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-05-15 15:28:11 +0100

    event model

---
+++ server/models/Event.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    default: '',
+    required: 'Title cannot be blank'
+  },
+  description: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  startDate: {
+    type: Date,
+    default: Date.now,
+    required: [true, 'Must have a start date'],
+  },
+  endData: {
+    type: Date,
+    default: Date.now,
+    required: [true, 'The End Data is required'],
+  },
+  participates: {
+    type: Number,
+    max: 15,
+  }
+});
+
+const Event = mongoose.model('Event', EventSchema);
+
+module.exports = Event;
