4442fbfcd46942294214a372fa7499de23e9176f
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-05-16 12:30:56 +0100

    .

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const { isLoggedIn } = require('../middlewares')
 const router = express.Router();
+const User = require('../models/User');
+
+router.get('/', (req, res, next) => {
+  res.render('paws/home-page');
+})
 
 router.get('/secret', isLoggedIn, (req, res, next) => {
   res.json({
@@ -8,5 +13,28 @@ router.get('/secret', isLoggedIn, (req, res, next) => {
     user: req.user
   });
 });
+
+// Route protected for logged in user
+router.get('/profile', isLoggedIn, (req, res, next) => {
+  User.find({ user: req.user._id })
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.json(err);
+    })
+})
+
+router.post('/profile', isLoggedIn, (req, res, next) => {
+  Profile.create({
+    _user: req.user._id,
+  })
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.json(err);
+    })
+})
 
 module.exports = router;
