01b0fdd8ba795fde4c067d5f921b657f7c7313e8
Doris <dorisc.santos@gmail.com>
2019-05-16 16:17:37 +0100

    new changes

--- server/app.js
+++ server/app.js
@@ -50,7 +50,7 @@ require('./passport')(app)
 
 app.use('/api', require('./routes/index'))
 app.use('/api', require('./routes/auth'))
-app.use('/api/pubs', require('./routes/pubs'))
+app.use('/api/pubCrawls', require('./routes/pubCrawls'))
 
 
 // For any routes that starts with "/api", catch 404 and forward to error handler
