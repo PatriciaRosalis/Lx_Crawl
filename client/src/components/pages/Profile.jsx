ab11e0a2b75473d18786f687771e6fe011913256
Doris <dorisc.santos@gmail.com>
2019-05-20 17:10:36 +0100

    profile update changes

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import api from '../../api';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import axios from 'axios';
+import '../../../src/profile.scss';
 
 export default class Profile extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       user: []
@@ -14,20 +15,60 @@ export default class Profile extends Component {
   render() {
     return (
       <div className="Profile">
-        <img src="../beer.svg" alt=""/>
-        <h2>Name</h2>
-        <h2>My routes</h2>
-        <ul>
-          <Link to="/:pubCrawlId"><button>Reuse</button></Link>
-          <Link to="/:pubCrawlId"><button>Reuse</button></Link>
-          <Link to="/:pubCrawlId"><button>Reuse</button></Link>
-        </ul>
-        <h2>My PubCrawls</h2>
+
+        <div className="_container">
+          <div className="_1container">
+            <img className="img" src="../beer.svg" alt="" />
+            <h4>Name</h4>
+          </div>
+          <div className="_2container">
+            <h5>My Routes</h5>
+              <p>Lorem ipsum <Link to="/:pubCrawlId"><button className="btn">REUSE</button></Link></p>
+              <p>Lorem ipsum <Link to="/:pubCrawlId"><button className="btn">REUSE</button></Link></p>
+              <p>Lorem ipsum <Link to="/:pubCrawlId"><button className="btn">REUSE</button></Link></p>
+          </div>
+        </div>
+
+        <div className="_3container">
+          <div>
+            
+            <h4>My Pub Crawls</h4>
+            <div className="demo-card-square mdl-card mdl-shadow--2dp">
+              <div className="mdl-card__title mdl-card--expand">
+                <h3 className="mdl-card__title-text">Lorem ipsum</h3>
+              </div>
+              <div className="mdl-card__supporting-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aenan convallis.
+              </div>
+              <div className="mdl-card__actions mdl-card--border">
+                <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                  View Updates</a>
+              </div>
+            </div>
+
+            <h4>Public Pub Crawls</h4>
+            <div className="demo-card-square mdl-card mdl-shadow--2dp">
+              <div className="mdl-card__title mdl-card--expand">
+                <h3 className="mdl-card__title-text">Lorem ipsum</h3>
+              </div>
+              <div className="mdl-card__supporting-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aenan convallis.
+              </div>
+              <div className="mdl-card__actions mdl-card--border">
+                <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                  View Updates</a>
+              </div>
+            </div>
+          
+          </div>
+        </div>
       </div>
     )
   }
   componentDidMount() {
-    axios.get('http://localhost:5000/api/profile')
+    axios.get('http://localhost:5000/api')
       .then(users => {
         console.log(users)
         this.setState({
