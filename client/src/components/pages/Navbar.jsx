f6a773c73b1e6b63016154c305964a04135914fe
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-05-20 20:49:01 +0100

    navbar ok

--- client/src/components/pages/Navbar.jsx
+++ client/src/components/pages/Navbar.jsx
@@ -4,30 +4,23 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div className="Navbar">
-      <div className="demo-layout-waterfall mdl-layout mdl-js-layout ">
-        <header className=" mdl-layout__header--waterfall">
+      <div className="mdl-layout mdl-js-layout">
+        <header className="mdl-layout__header mdl-layout__header--scroll">
           <div className="mdl-layout__header-row">
             <span className="mdl-layout-title">LxCrawl</span>
             <div className="mdl-layout-spacer"></div>
-            <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
-              <div className="mdl-textfield__expandable-holder">
-                <input className="mdl-textfield__input" type="text" name="sample" id="waterfall-exp" />
-              </div>
-            </div>
           </div>
-
         </header>
         <div className="mdl-layout__drawer">
           <span className="mdl-layout-title"><img className="title-nav" src="../title.png" alt="title" /></span>
           <nav className="mdl-navigation">
-            <Link className="mdl-navigati on__link" to="/profile">Profile</Link>
-            <Link className="mdl-navigation__link" to="/add-pubcrawl">Add PubCrawl</Link>
-            {/* <Link className="mdl-navigation__link" to="">Join PubCrawl</Link> */}
+          <Link className="mdl-navigation__link" to="">Profile</Link>
+          <Link className="mdl-navigation__link" to="">Add Pub</Link>
           </nav>
         </div>
-        <main className="mdl-layout__content">
-          <div className="page-content"></div>
-        </main>
+        {/* <main className="mdl-layout__content">
+    <div className="page-content"><!-- Your content goes here --></div>
+  </main> */}
       </div>
     </div>
   )
