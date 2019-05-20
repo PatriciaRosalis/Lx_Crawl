d317a4cd4b6cd50f89c4ecddc303b06ed4b2b526
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-05-20 16:10:57 +0100

    .

---
+++ client/src/components/pages/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="demo-layout-waterfall mdl-layout mdl-js-layout ">
+        <header className=" mdl-layout__header--waterfall">
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">LxCrawl</span>
+            <div className="mdl-layout-spacer"></div>
+            <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+              <div className="mdl-textfield__expandable-holder">
+                <input className="mdl-textfield__input" type="text" name="sample" id="waterfall-exp" />
+              </div>
+            </div>
+          </div>
+
+        </header>
+        <div className="mdl-layout__drawer">
+          <span className="mdl-layout-title"><img className="title-nav" src="../title.png" alt="title" /></span>
+          <nav className="mdl-navigation">
+            <Link className="mdl-navigation__link" to="/profile">Profile</Link>
+            <Link className="mdl-navigation__link" to="/">Add PubCrawl</Link>
+            {/* <Link className="mdl-navigation__link" to="">Join PubCrawl</Link> */}
+          </nav>
+        </div>
+        <main className="mdl-layout__content">
+          <div className="page-content"></div>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
