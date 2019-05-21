62582e3501bf9fc22b373cd2e8d1d2759df3532f
Joana Duarte <joanarpcribeiro@yahoo.com>
2019-05-21 11:58:48 +0100

    First design of AddPubCrawl and fixed addPubCrawl route

--- client/src/components/pages/AddPubCrawl.jsx
+++ client/src/components/pages/AddPubCrawl.jsx
@@ -3,6 +3,7 @@ import api from '../../api';
 import ReactDom from 'react-dom'
 import axios from 'axios';
 import Navbar from './Navbar'
+import '../../styles/pubcrawlForms.scss';
 
 
 export default class AddPubCrawl extends Component {
@@ -88,28 +89,96 @@ export default class AddPubCrawl extends Component {
   render() {
     return (
       <div className="AddPubCrawl">
-        <Navbar /> 
-        <br /> 
-        
+        {/* <Navbar />  */}
+        <br />
         <h2>Add Pub Crawl</h2>
-        <form> 
-          Name: <input type="text" value={this.state.name} name="name" onChange={this.handleInputChange} /> <br />
-          Comments: <textarea value={this.state.comments} name="comments" cols="30" rows="10" onChange={this.handleInputChange} ></textarea> <br />
-          Start Date: <input type="date" value={this.state.startDate} name="startDate" onChange={this.handleInputChange} /> <br /> 
-          End Date: <input type="date" value={this.state.endDate} name="endDate" onChange={this.handleInputChange} /> <br /> 
-          Participants: <input type="number" value={this.state.participants} name="participants" onChange={this.handleInputChange} /> <br /> 
-          <hr />
-          <h3>Places</h3>
-          {this.state.places.map((place,i) => <div key={i}>
-            Pub name : <input value={place.namePub} onChange={e => this.changeNamePub(e,i)} /> <br/>
-            Address : <input value={place.address} onChange={e => this.changeAddress(e,i)}/> <br/>
-            <br/>
-          </div>)}
-          <button onClick={(e) => this.handleClick(e)}>Create Pub Crawl</button>
-        </form>
-        {this.state.message && <div className="info">
-          {this.state.message}
-        </div>}
+        <div className="containerAddPub">
+          <form action="action_page.php">
+            <div className="row">
+              <div className="col-25">
+                <label for="fname" className="label">Name</label>
+              </div>
+              <div className="col-75">
+                {/* <input type="text" value={this.state.name} onChange={this.handleInputChange} id="fname" placeholder="Pub Crawl Name..." /> */}
+                <input id="pubName" type="text" value={this.state.name} name="name" onChange={this.handleInputChange} placeholder="Pub Crawl Name..." />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label for="sdate" className="label">Start Date</label>
+              </div>
+              <div className="col-75">
+                <input id="sdate"  name="startDate" type="date" value={this.state.startDate}  onChange={this.handleInputChange} />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label for="edate" className="label">End Date</label>
+              </div>
+              <div className="col-75">
+                <input id="edate" name="endDate" type="date" value={this.state.endDate} onChange={this.handleInputChange} />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label for="participant" className="label">Participant:</label>
+              </div>
+              <div className="col-75">
+                <input id="participant" type="number" value={this.state.participants} name="participants" onChange={this.handleInputChange} />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label for="comments" className="label">Comments</label>
+              </div>
+              <div className="col-75">
+                <textarea id="comments" name="comments" placeholder="Write something.." value={this.state.comments} /*name="comments"*/ cols="30" rows="10" onChange={this.handleInputChange}></textarea>
+              </div>
+            </div>
+            <h3>Places</h3>
+            <div className="row">
+              {this.state.places.map((place,i) => <div key={i}>
+                <div className="col-25">
+                  <label for="pubname" className="label" >Pub Name</label>
+                </div>
+                <div className="col-75">
+                  <input value={place.namePub} onChange={e => this.changeNamePub(e,i)} />
+                </div>
+                <div className="col-25">
+                  <label for="address" className="label" >Address </label>
+                </div>
+                <div className="col-75">
+                  <input value={place.address} onChange={e => this.changeAddress(e,i)}/>
+                </div>
+              </div>)}
+            </div>
+            <button id="createPub" onClick={(e) => this.handleClick(e)}>Create Pub Crawl</button>
+          </form>
+          {this.state.message && <div className="info">
+            {this.state.message}
+          </div>}
+        </div>
+
+
+        {/* <h2>Add Pub Crawl</h2> */}
+        {/* <form>  */}
+          {/* Name: <input type="text" value={this.state.name} name="name" onChange={this.handleInputChange} /> <br /> */}
+          {/* Comments: <textarea value={this.state.comments} name="comments" cols="30" rows="10" onChange={this.handleInputChange} ></textarea> <br /> */}
+          {/* Start Date: <input type="date" value={this.state.startDate} name="startDate" onChange={this.handleInputChange} /> <br />  */}
+          {/* End Date: <input type="date" value={this.state.endDate} name="endDate" onChange={this.handleInputChange} /> <br />  */}
+          {/* Participants: <input type="number" value={this.state.participants} name="participants" onChange={this.handleInputChange} /> <br />  */}
+          {/* <hr /> */}
+          {/* <h3>Places</h3> */}
+          {/* {this.state.places.map((place,i) => <div key={i}> */}
+            {/* Pub name : <input value={place.namePub} onChange={e => this.changeNamePub(e,i)} /> <br/> */}
+            {/* Address : <input value={place.address} onChange={e => this.changeAddress(e,i)}/> <br/> */}
+            {/* <br/> */}
+          {/* </div>)} */}
+          {/* <button onClick={(e) => this.handleClick(e)}>Create Pub Crawl</button> */}
+        {/* </form> */}
+        {/* {this.state.message && <div className="info"> */}
+          {/* {this.state.message} */}
+        {/* </div>} */}
       </div>
     );
   }
