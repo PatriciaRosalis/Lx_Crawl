5a77c920c57f7de28b97731930a57bac852cc811
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-05-21 12:57:42 +0100

    add pub style

--- client/src/components/pages/AddPubCrawl.jsx
+++ client/src/components/pages/AddPubCrawl.jsx
@@ -11,7 +11,7 @@ export default class AddPubCrawl extends Component {
     super(props)
     this.state = {
       name: "",
-      places: [{namePub: "Castro", address: "Rua de test"}],
+      places: [{ namePub: "Castro", address: "Rua de test" }],
       comments: "",
       startDate: new Date(),
       endDate: new Date(),
@@ -34,13 +34,13 @@ export default class AddPubCrawl extends Component {
     console.log(this.state.name, this.state.description)
     let data = {
       name: this.state.name,
-      places: this.state.places, 
+      places: this.state.places,
       comments: this.state.comments,
       startDate: this.state.startDate,
       endDate: this.state.endDate,
       participants: this.state.participants,
     }
-    api.addPubCrawl( data )
+    api.addPubCrawl(data)
       .then(result => {
         console.log('SUCCESS!')
         this.setState({
@@ -63,7 +63,7 @@ export default class AddPubCrawl extends Component {
   changeNamePub(e, i) {
     let copyPlaces = [...this.state.places] // Create a copy of the state
     copyPlaces[i].namePub = e.target.value // Change the value at position i
-    if (i === this.state.places.length-1) { // If we are modifying the last element, add an extra place
+    if (i === this.state.places.length - 1) { // If we are modifying the last element, add an extra place
       copyPlaces.push({
         namePub: "",
         address: ""
@@ -76,7 +76,7 @@ export default class AddPubCrawl extends Component {
   changeAddress(e, i) {
     let copyPlaces = [...this.state.places] // Create a copy of the state
     copyPlaces[i].address = e.target.value // Change the value at position i
-    if (i === this.state.places.length-1) { // If we are modifying the last element, add an extra place
+    if (i === this.state.places.length - 1) { // If we are modifying the last element, add an extra place
       copyPlaces.push({
         namePub: "",
         address: ""
@@ -93,66 +93,66 @@ export default class AddPubCrawl extends Component {
         <br />
         <h2>Add Pub Crawl</h2>
         <div className="containerAddPub">
-          <form action="action_page.php">
-            <div className="row">
+          <form action="action_page.php" className="form-act">
+            <div className="space">
               <div className="col-25">
-                <label for="fname" className="label">Name</label>
+                <label for="fname" className="label color-form">PubCrawl Name</label>
               </div>
               <div className="col-75">
                 {/* <input type="text" value={this.state.name} onChange={this.handleInputChange} id="fname" placeholder="Pub Crawl Name..." /> */}
-                <input id="pubName" type="text" value={this.state.name} name="name" onChange={this.handleInputChange} placeholder="Pub Crawl Name..." />
+                <input className="mdl-textfield__input" id="pubName" type="text" value={this.state.name} name="name" onChange={this.handleInputChange} placeholder="Pub Crawl Name..." />
               </div>
             </div>
-            <div className="row">
+            <div className="space">
               <div className="col-25">
-                <label for="sdate" className="label">Start Date</label>
+                <label for="sdate" className="label color-form">Start Date</label>
               </div>
               <div className="col-75">
-                <input id="sdate"  name="startDate" type="date" value={this.state.startDate}  onChange={this.handleInputChange} />
+                <input className="mdl-textfield__input" id="sdate" name="startDate" type="date" value={this.state.startDate} onChange={this.handleInputChange} />
               </div>
             </div>
-            <div className="row">
+            <div className="space">
               <div className="col-25">
-                <label for="edate" className="label">End Date</label>
+                <label for="edate" className="label color-form">End Date</label>
               </div>
               <div className="col-75">
-                <input id="edate" name="endDate" type="date" value={this.state.endDate} onChange={this.handleInputChange} />
+                <input className="mdl-textfield__input" id="edate" name="endDate" type="date" value={this.state.endDate} onChange={this.handleInputChange} />
               </div>
             </div>
-            <div className="row">
+            <div className="space">
               <div className="col-25">
-                <label for="participant" className="label">Participant:</label>
+                <label for="participant" className="label color-form">Participant:</label>
               </div>
               <div className="col-75">
-                <input id="participant" type="number" value={this.state.participants} name="participants" onChange={this.handleInputChange} />
+                <input className="mdl-textfield__input" id="participant" type="number" value={this.state.participants} name="participants" onChange={this.handleInputChange} />
               </div>
             </div>
-            <div className="row">
+            <div className="space">
               <div className="col-25">
-                <label for="comments" className="label">Comments</label>
+                <label for="comments" className="label color-form">Comments</label>
               </div>
               <div className="col-75">
-                <textarea id="comments" name="comments" placeholder="Write something.." value={this.state.comments} /*name="comments"*/ cols="30" rows="10" onChange={this.handleInputChange}></textarea>
+                <textarea id="comments" name="comments" placeholder="Comments" value={this.state.comments} /*name="comments"*/ cols="40" rows="3" onChange={this.handleInputChange}></textarea>
               </div>
             </div>
-            <h3>Places</h3>
-            <div className="row">
-              {this.state.places.map((place,i) => <div key={i}>
+            <div className="space ">
+            <h4>Places</h4>
+              {this.state.places.map((place, i) => <div key={i}>
                 <div className="col-25">
-                  <label for="pubname" className="label" >Pub Name</label>
+                  <label className="mdl-textfield__label " for="pubname" className="label color-form" >Pub Name</label>
                 </div>
                 <div className="col-75">
-                  <input value={place.namePub} onChange={e => this.changeNamePub(e,i)} />
+                  <input className="mdl-textfield__input" value={place.namePub} onChange={e => this.changeNamePub(e, i)} />
                 </div>
                 <div className="col-25">
-                  <label for="address" className="label" >Address </label>
+                  <label className="mdl-textfield__label" for="address" className="label color-form " >Address </label>
                 </div>
                 <div className="col-75">
-                  <input value={place.address} onChange={e => this.changeAddress(e,i)}/>
+                  <input className="mdl-textfield__input" value={place.address} onChange={e => this.changeAddress(e, i)} />
                 </div>
               </div>)}
             </div>
-            <button id="createPub" onClick={(e) => this.handleClick(e)}>Create Pub Crawl</button>
+            <button className="btn" id="createPub" onClick={(e) => this.handleClick(e)}>Create </button>
           </form>
           {this.state.message && <div className="info">
             {this.state.message}
@@ -162,22 +162,22 @@ export default class AddPubCrawl extends Component {
 
         {/* <h2>Add Pub Crawl</h2> */}
         {/* <form>  */}
-          {/* Name: <input type="text" value={this.state.name} name="name" onChange={this.handleInputChange} /> <br /> */}
-          {/* Comments: <textarea value={this.state.comments} name="comments" cols="30" rows="10" onChange={this.handleInputChange} ></textarea> <br /> */}
-          {/* Start Date: <input type="date" value={this.state.startDate} name="startDate" onChange={this.handleInputChange} /> <br />  */}
-          {/* End Date: <input type="date" value={this.state.endDate} name="endDate" onChange={this.handleInputChange} /> <br />  */}
-          {/* Participants: <input type="number" value={this.state.participants} name="participants" onChange={this.handleInputChange} /> <br />  */}
-          {/* <hr /> */}
-          {/* <h3>Places</h3> */}
-          {/* {this.state.places.map((place,i) => <div key={i}> */}
-            {/* Pub name : <input value={place.namePub} onChange={e => this.changeNamePub(e,i)} /> <br/> */}
-            {/* Address : <input value={place.address} onChange={e => this.changeAddress(e,i)}/> <br/> */}
-            {/* <br/> */}
-          {/* </div>)} */}
-          {/* <button onClick={(e) => this.handleClick(e)}>Create Pub Crawl</button> */}
+        {/* Name: <input type="text" value={this.state.name} name="name" onChange={this.handleInputChange} /> <br /> */}
+        {/* Comments: <textarea value={this.state.comments} name="comments" cols="30" rows="10" onChange={this.handleInputChange} ></textarea> <br /> */}
+        {/* Start Date: <input type="date" value={this.state.startDate} name="startDate" onChange={this.handleInputChange} /> <br />  */}
+        {/* End Date: <input type="date" value={this.state.endDate} name="endDate" onChange={this.handleInputChange} /> <br />  */}
+        {/* Participants: <input type="number" value={this.state.participants} name="participants" onChange={this.handleInputChange} /> <br />  */}
+        {/* <hr /> */}
+        {/* <h3>Places</h3> */}
+        {/* {this.state.places.map((place,i) => <div key={i}> */}
+        {/* Pub name : <input value={place.namePub} onChange={e => this.changeNamePub(e,i)} /> <br/> */}
+        {/* Address : <input value={place.address} onChange={e => this.changeAddress(e,i)}/> <br/> */}
+        {/* <br/> */}
+        {/* </div>)} */}
+        {/* <button onClick={(e) => this.handleClick(e)}>Create Pub Crawl</button> */}
         {/* </form> */}
         {/* {this.state.message && <div className="info"> */}
-          {/* {this.state.message} */}
+        {/* {this.state.message} */}
         {/* </div>} */}
       </div>
     );
