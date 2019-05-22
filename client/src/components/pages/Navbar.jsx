b34ca2dee7b8f17b4b2c7128660cd2e0748b0949
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-05-22 17:20:55 +0100

    WIP navbar

--- client/src/components/pages/Navbar.jsx
+++ client/src/components/pages/Navbar.jsx
@@ -4,24 +4,22 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div className="Navbar">
+      <nav className="navbar navbar-expand-lg navbar-dark ">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <Link className="navbar-brand" to="#">Navbar</Link>
-
-  <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li className="nav-item active">
-        <Link className="nav-link" to="/profile">Profile <span className="sr-only">(current)</span></Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/add-pubcrawl">Add Pubcrawl</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/profile">Profile <span className="sr-only">(current)</span></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/add-pubcrawl">Add Pubcrawl</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
       {/* <div className="">
         <header className="">
