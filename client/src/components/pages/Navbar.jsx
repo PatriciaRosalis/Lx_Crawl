c778910a8047d04b1ecc0f62d22810a843688738
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-05-22 14:58:40 +0100

    .

--- client/src/components/pages/Navbar.jsx
+++ client/src/components/pages/Navbar.jsx
@@ -5,11 +5,11 @@ function Navbar() {
   return (
     <div className="Navbar">
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <Link class="navbar-brand" to="#">Navbar</Link>
+  <Link class="navbar-brand" to="#">LxCrawl</Link>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
