9ade0bb8a5a10adf42ae843005e76c7ea1304b6d
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-05-22 15:47:02 +0100

    WIP Bootstrap PubC

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -3,7 +3,6 @@ import api from '../../api';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import axios from 'axios';
 import Navbar from './Navbar'
-// import '../../../src/profile.scss';
 
 export default class Profile extends Component {
   constructor(props) {
@@ -14,48 +13,44 @@ export default class Profile extends Component {
   }
 
 
-
   render() {
     return (
       <div className="Profile">
-        {/* <Navbar /> */}
+        <Navbar />
         <div className="_container">
           <div className="_1container">
             <img className="img" src="../beer.svg" alt="" />
-            { api.getLocalStorageUser() && <h4>{api.getLocalStorageUser().username}</h4>} 
+            {api.getLocalStorageUser() && <h4>{api.getLocalStorageUser().username}</h4>}
           </div>
           <div className="_2container">
-          <Link to={'/add-pubcrawl'}><button>
-          New Pub Crawl</button></Link>
+            <Link to={'/add-pubcrawl'}><button>
+              New Pub Crawl</button></Link>
           </div>
         </div>
-
-
-
         <div className="_3container">
           <div>
-            
+
             <h4>My Pub Crawls</h4>
-            {this.state.pubCrawls && this.state.pubCrawls.map(oneCrawl => 
+            {this.state.pubCrawls && this.state.pubCrawls.map(oneCrawl =>
               <div key={oneCrawl._id}>
 
-              <div className="demo-card-square mdl-card mdl-shadow--2dp">
-              <div className="mdl-card__title mdl-card--expand">
-                <h3 className="mdl-card__title-text">{oneCrawl.name}</h3>
+                <div className="demo-card-square mdl-card mdl-shadow--2dp">
+                  <div className="mdl-card__title mdl-card--expand">
+                    <h3 className="mdl-card__title-text">{oneCrawl.name}</h3>
+                  </div>
+                  <div className="mdl-card__supporting-text">
+                    {oneCrawl.startDate}
+                  </div>
+                  <div className="mdl-card__actions mdl-card--border">
+                    <Link to={`/edit-pubCrawl/${oneCrawl._id}`} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                      Edit</Link>
+                    <Link to={`/pubcrawl-detail/${oneCrawl._id}`} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                      View Details</Link>
+                  </div>
+                </div>
               </div>
-              <div className="mdl-card__supporting-text">
-                {oneCrawl.startDate}
-              </div>
-              <div className="mdl-card__actions mdl-card--border">
-                <Link to={`/edit-pubCrawl/${oneCrawl._id}`} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                  Edit</Link>
-                <Link to={`/pubcrawl-detail/${oneCrawl._id}`} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                  View Details</Link>
-              </div>
-            </div>
-            </div>
             )
-          }
+            }
 
             {/* // <h4>Previous Pub Crawls</h4>
             // <div className="demo-card-square mdl-card mdl-shadow--2dp">
@@ -71,7 +66,7 @@ export default class Profile extends Component {
             //       Reuse</Link>
             //   </div>
             // </div> */}
-          
+
           </div>
         </div>
       </div>
@@ -79,20 +74,20 @@ export default class Profile extends Component {
   }
   componentDidMount() {
     api.getAllPubCrawlsUser()
-    .then(pubCrawls => {
-      this.setState({
-        pubCrawls: pubCrawls
+      .then(pubCrawls => {
+        this.setState({
+          pubCrawls: pubCrawls
+        })
+        console.log(this.state.pubCrawls)
       })
-      console.log(this.state.pubCrawls)
-    })
-  //   console.log(api.getLocalStorageUser())
-  //   api.get/*something from api.js*/ */('http://localhost:5000/api')
-  //     .then(users => {
-  //       console.log(users)
-  //       this.setState({
-  //         users: users
-  //       })
-  //     })
-  //     .catch(err => console.log(err))
+    //   console.log(api.getLocalStorageUser())
+    //   api.get/*something from api.js*/ */('http://localhost:5000/api')
+    //     .then(users => {
+    //       console.log(users)
+    //       this.setState({
+    //         users: users
+    //       })
+    //     })
+    //     .catch(err => console.log(err))
   }
 }
