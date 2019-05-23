68b53de9e3398a380fc6dfb30eca3ca88446f546
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-05-23 15:28:14 +0100

    icon share

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,92 +1,101 @@
-import React, { Component } from 'react';
-import api from '../../api';
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import axios from 'axios';
-import Navbar from './Navbar';
-// const { isLoggedIn } = require('../middlewares')
-
+import React, { Component } from "react";
+import { FacebookShareButton, WhatsappShareButton, EmailShareButton } from "react-share";
+import { FacebookIcon, WhatsappIcon, EmailIcon } from 'react-share';
+import api from "../../api";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default class Profile extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      user: []
-    }
+    user: []
+  }
   }
   handleInputChange(event) {
     this.setState({
       [event.target.name]: event.target.value
-    })
+    });
   }
   handleLogout(e) {
-    e.preventDefault()
-    api.logout()
+    e.preventDefault();
+    api
+      .logout()
       .then(result => {
-        console.log('SUCCESS!')
-        this.props.history.push("/") // Redirect to the home page
+        console.log("SUCCESS!");
+        this.props.history.push("/"); // Redirect to the home page
       })
-      .catch(err => this.setState({
-        message: err.toString()
-      }))
+      .catch(err =>
+        this.setState({
+          message: err.toString()
+        })
+      );
   }
   handleClick(data) {
     // console.log("data", data)
     const pubId = data;
-    api.deletePubCrawl(data)
-      .then(result => {
-        console.log('SUCCESS - DELETE!')
-        const pubCrawls = this.state.pubCrawls.filter(x => x._id !== pubId);
-        this.setState({
-          message: `Your Pub Crawl has been deleted`,
-          pubCrawls: pubCrawls
-        })
-      })
+    api.deletePubCrawl(data).then(result => {
+      console.log("SUCCESS - DELETE!");
+      const pubCrawls = this.state.pubCrawls.filter(x => x._id !== pubId);
+      this.setState({
+        message: `Your Pub Crawl has been deleted`,
+        pubCrawls: pubCrawls
+      });
+    });
   }
 
   render() {
     return (
-      <div className="Profile" >
-        <Navbar />
-        <div className="_container">
-          <div className="_1container">
-            <img className="img" src="../beer.svg" alt="" />
-            {api.getLocalStorageUser() && <h4>{api.getLocalStorageUser().username}</h4>}
-          </div>
-          <div className="_2container">
-            <Link to={'/add-pubcrawl'} className="btns">
-              New Pub Crawl</Link>
-            <br />
-            <button className="btns" onClick={(e) => this.handleLogout(e)}>
-              Logout</button>
+      <React.Fragment>
+        <div className="Profile">
+          <Navbar />
+          <div className="_container">
+            <div className="_1container">
+              <img className="img" src="../beer.svg" alt="" />
+              {api.getLocalStorageUser() && (
+                <h4>{api.getLocalStorageUser().username}</h4>
+              )}
+            </div>
+            <div className="_2container">
+              <Link to={"/add-pubcrawl"} className="btns">
+                New Pub Crawl
+              </Link>
+              <br />
+              <button className="btns" onClick={e => this.handleLogout(e)}>
+                Logout
+              </button>
+            </div>
           </div>
         </div>
-
-
-
         <div className="_3container">
           <div>
-
             <h4>My Pub Crawls</h4>
-            {this.state.pubCrawls && this.state.pubCrawls.map(oneCrawl =>
-              <div key={oneCrawl._id}>
-
-                <div className="card cards" style={{ width: "18rem" }}>
-                  <div className="card-body">
-                    <h5 className="card-title ">{oneCrawl.name}</h5>
-                    <p className="card-text">{oneCrawl.startDate}</p>
-                    <Link to={`/edit-pubCrawl/${oneCrawl._id}`} className="btns">Edit</Link>
-                    <Link to={`/pubcrawl-detail/${oneCrawl._id}`} className="btns">View Details</Link>
-                    <button className="btns" onClick={() => this.handleClick(oneCrawl._id)}>Delete</button>
+            {this.state.pubCrawls &&
+              this.state.pubCrawls.map(oneCrawl => (
+                <div key={oneCrawl._id}>
+                  <div className="cards" style={{ width: "18rem" }}>
+                    <div className="card-body edit-profile-icon">
+                      <h5 className="card-title ">{oneCrawl.name}</h5>
+                      <p className="card-text">{oneCrawl.startDate}</p>
+                      <Link to={`/edit-pubCrawl/${oneCrawl._id}`} className="btns">Edit</Link>
+                      <Link to={`/pubcrawl-detail/${oneCrawl._id}`} className="btns"> View Details</Link>
+                      <button className="btns" onClick={() => this.handleClick(oneCrawl._id)}>Delete</button>
+                      <FacebookShareButton url={"shareUrl"} quote={"title"} className="Demo_some_network_share-button">
+                        <FacebookIcon size={32} round />
+                      </FacebookShareButton>
+                      <WhatsappShareButton url={"shareUrl"} quote={"title"} className="Demo_some_network_share-button"><WhatsappIcon size={32} round /></WhatsappShareButton>
+                      <EmailShareButton url={"shareUrl"} quote={"title"} className="Demo_some_network_share-button"><EmailIcon size={32} round /></EmailShareButton>
+                    </div>
                   </div>
                 </div>
+              ))}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 
-              </div>
-            )}
-
-
-
-
-            {/* // <h4>Previous Pub Crawls</h4>
+  /* // <h4>Previous Pub Crawls</h4>
             // <div className="demo-card-square mdl-card mdl-shadow--2dp">
             //   <div className="mdl-card__title mdl-card--expand">
             //     <h3 className="mdl-card__title-text">Lorem ipsum</h3>
@@ -99,33 +108,28 @@ export default class Profile extends Component {
             //     <Link to='/edit-pubCrawl/:pubCrawlId' className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
             //       Reuse</Link>
             //   </div>
-            // </div> */}
+            // </div> */
 
-          </div>
-        </div>
-      </div>
-    )
-  }
   componentDidMount() {
     if (api.isLoggedIn()) {
-      api.getAllPubCrawlsUser()
-        .then(pubCrawls => {
-          this.setState({
-            pubCrawls: pubCrawls
-          })
-          console.log(this.state.pubCrawls)
-        })
+      api.getAllPubCrawlsUser().then(pubCrawls => {
+        this.setState({
+          pubCrawls: pubCrawls
+        });
+        console.log(this.state.pubCrawls);
+      });
     } else {
-      this.props.history.push("/")
+      this.props.history.push("/");
     }
-    //   console.log(api.getLocalStorageUser())
-    //   api.get/*something from api.js*/ */('http://localhost:5000/api')
-    //     .then(users => {
-    //       console.log(users)
-    //       this.setState({
-    //         users: users
-    //       })
-    //     })
-    //     .catch(err => console.log(err))
   }
 }
+
+//   console.log(api.getLocalStorageUser())
+//   api.get/something from api.js/ */('http://localhost:5000/api')
+//     .then(users => {
+//       console.log(users)
+//       this.setState({
+//         users: users
+//       })
+//     })
+//     .catch(err => console.log(err))
