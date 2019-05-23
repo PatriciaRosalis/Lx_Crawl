972da2e15960f78e58953c310bd35b1c0a49c11d
Joana Duarte <joanarpcribeiro@yahoo.com>
2019-05-23 18:43:12 +0100

    Auto complete Add Pub crawl and edit pub crawl

--- client/src/components/pages/EditPubCrawl.jsx
+++ client/src/components/pages/EditPubCrawl.jsx
@@ -1,6 +1,5 @@
 /////client/src/components/EditPubCrawl/////////////////
 
-
 import React, { Component } from 'react';
 import api from '../../api';
 import Navbar from './Navbar'
@@ -11,7 +10,7 @@ export default class EditPubCrawl extends Component {
     super(props)
     this.state = {
       name: "",
-      places: [{ namePub: "Castro", address: "Rua de test" }],
+      places: [],
       comments: "",
       startDate: new Date(),
       endDate: new Date(),
@@ -20,7 +19,6 @@ export default class EditPubCrawl extends Component {
     }
     this.handleInputChange = this.handleInputChange.bind(this)
     this.changeNamePub = this.changeNamePub.bind(this)
-    //this.changeAddress = this.changeAddress.bind(this)
   }
 
   handleInputChange(event) {
@@ -56,9 +54,11 @@ export default class EditPubCrawl extends Component {
       })
       .catch(err => this.setState({ message: err.toString() }))
   }
-  changeNamePub(e, i) {
+  changeNamePub(place, i) {
     let copyPlaces = [...this.state.places] // Create a copy of the state
-    copyPlaces[i].namePub = e.target.value // Change the value at position i
+
+    copyPlaces[i].namePub = place.text // Change the value at position i
+    copyPlaces[i].address = place.place_name // Change the value at position i
     if (i === this.state.places.length - 1) { // If we are modifying the last element, add an extra place
       copyPlaces.push({
         namePub: "",
@@ -69,19 +69,6 @@ export default class EditPubCrawl extends Component {
       places: copyPlaces
     })
   }
-  // changeAddress(e, i) {
-  //   let copyPlaces = [...this.state.places] // Create a copy of the state
-  //   copyPlaces[i].address = e.target.value // Change the value at position i
-  //   if (i === this.state.places.length - 1) { // If we are modifying the last element, add an extra place
-  //     copyPlaces.push({
-  //       namePub: "",
-  //       address: ""
-  //     })
-  //   }
-  //   this.setState({
-  //     places: copyPlaces
-  //   })
-  // }
   render() {
     return (
       <div className="EditPubCrawl">
@@ -114,12 +101,8 @@ export default class EditPubCrawl extends Component {
           {this.state.places.map((place, i) => <div className="form-group col-md-6" key={i}>
             <div className="">
               <label className="mdl-textfield__label" htmlFor="participants" className="label color-form">Pub name:</label>
-              <input className="mdl-textfield__input form-control"  value={place.namePub} onChange={e => this.changeNamePub(e, i)} /> <br />
+              <AutocompletePlace initialValue={place.namePub} onSelect={place => this.changeNamePub(place, i)}  />
             </div>
-            {/* <div className="">
-              <label className="mdl-textfield__label" htmlFor="address" className="label color-form"> Address:</label>
-              <input className="mdl-textfield__input form-control" value={place.address} onChange={e => this.changeAddress(e, i)} /> <br />
-            </div> */}
           </div>)}
           <button className="btns" onClick={(e) => this.handleClick(e)}>Edit Pub Crawl</button>
           </div>
@@ -149,83 +132,3 @@ export default class EditPubCrawl extends Component {
       })
   }
 }
-
-
-// import React, { Component } from 'react';
-// import api from '../../api.js'
-
-// class EditPubCrawl extends Component {
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       name: this.props.pubCrawl.name,
-//       places: this.props.pubCrawl.places,
-//       comments: this.props.pubCrawl.comments,
-//       startDate: this.props.pubCrawl.startDate,
-//       endDate: this.props.pubCrawl.endDate,
-//       participants: this.props.pubCrawl.participants,
-//     }
-//   }
-
-
-//   handleFormSubmit = (event) => {
-//     event.preventDefault();
-//     const pubCrawlData = {
-//       name: this.state.name,
-//       places: this.state.places,
-//       comments: this.state.comments,
-//       startDate: this.state.startDate,
-//       endDate: this.state.endDate,
-//       participants: this.state.participants
-//     }
-
-
-
-//     api.put(`${this.props.pubCrawlId}`, { pubCrawlData })
-//     .then( () => {
-//         this.props.getThePubCrawl();
-//         // after submitting the form, redirect to '/projects'
-//         this.props.history.push('/my-pubcrawls');    
-//     })
-//     .catch( error => console.log(error) )
-//   }
-
-//   handleInputChange(e) {
-//     this.setState({
-//       [e.target.name]: e.target.value
-//     })
-//   }
-
-//   render(){
-//     return (
-//       <div>
-//         <hr />
-//         <h3>Edit form</h3>
-//         <form onSubmit={this.handleFormSubmit}>
-//           <label>Name:</label>
-//           <input type="text" value={this.state.name} name="name" onChange={this.handleInputChange} /> <br />
-//           <label>Comments:</label>
-//           <textarea value={this.state.comments} name="comments" cols="30" rows="10" onChange={this.handleInputChange} ></textarea> <br />
-//           <label>Start Date:</label>
-//           <input type="date" value={this.state.startDate} name="startDate" onChange={this.handleInputChange} /> <br /> 
-//           <label>End Date:</label>
-//           <input type="date" value={this.state.endDate} name="endDate" onChange={this.handleInputChange} /> <br /> 
-//           <label>Participants:</label>
-//           <input type="number" value={this.state.participants} name="participants" onChange={this.handleInputChange} /> <br /> 
-//           <hr />
-//           <h3>Places</h3>
-//           {this.state.places.map((place,i) => <div key={i}>
-//           <label>Pub name :</label>
-//           <input value={place.namePub} onChange={e => this.changeNamePub(e,i)} /> <br/>
-//           <label>Address :</label>
-//           <input value={place.address} onChange={e => this.changeAddress(e,i)}/> <br/>
-//             <br/>
-//           </div>)}
-//           <button onClick={(e) => this.handleClick(e)}>Edit Pub Crawl</button>
-//         </form>
-//       </div>
-//     )
-//   }
-// }
-
-// export default EditPubCrawl;
